7b311bc5b377ab05c743b31de8202f8cb8e58c5e
Conor Panuccio <conorpanu@hotmail.com>
2023-11-01 20:32:04 -0400

    finished makeing a bonary sort

--- binary_search.py
+++ binary_search.py
@@ -1,2 +1,22 @@
 # code your iterative algorithm here
 
+list = [5, 8, 12, 14, 19, 22, 27, 30 , 31]
+
+def binary_search(data, el):
+    first = 0
+    last = len(data)-1
+    found = False
+
+    while first <= last and not found:
+        mid = (first + last) // 2
+
+        if data[mid] == el:
+            found = True
+        else:
+            if el < data[mid]:
+                last = mid - 1
+            else:
+                first = mid + 1
+    return found
+
+print(binary_search(list, 12))
